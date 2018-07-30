eccd7af643dcf47814987f05037ebc3264dcc85a
AIE\s171496 <xeno.spam1993@gmail.com>
2018-07-30 14:29:13 +0930

    P2 do as P1 do.

--- Assets/Scripts/Player2/CameraControllerP2.cs
+++ Assets/Scripts/Player2/CameraControllerP2.cs
@@ -11,6 +11,12 @@ public class CameraControllerP2 : MonoBehaviour {
     public float m_MinPanX = -50.0f, m_MaxPanX = 50.0f;
     public float m_MinPanZ = -50.0f, m_MaxPanZ = 50.0f;
 
+    public float slerpSpeed = 50;
+
+    Vector3 position;
+
+    public bool changePosition;
+
     private void Awake()
     {
         m_controller = InputManager.Devices[1];
@@ -33,7 +39,7 @@ public class CameraControllerP2 : MonoBehaviour {
 
             transform.position = new Vector3(transform.position.x, zoom, transform.position.z);
         }
-        else
+        else if (changePosition == false)
         {
             this.transform.position += new Vector3(-m_controller.RightStickX, 0, -m_controller.RightStickY);
 
@@ -42,10 +48,20 @@ public class CameraControllerP2 : MonoBehaviour {
 
             transform.position = new Vector3(panX, transform.position.y, panZ);
         }
+
+        if (changePosition)
+        {
+            transform.position = Vector3.Slerp(transform.position, position, slerpSpeed);
+
+            if (Vector3.Distance(transform.position, position) <= 1)
+            {
+                changePosition = false;
+            }
+        }
     }
 
-    public void SetPosition(float x, float z)
+    public void MoveCameraTo(float x, float z)
     {
-        transform.position = new Vector3(x, transform.position.y, z);
+        position = new Vector3(x, transform.position.y, z);
     }
 }
