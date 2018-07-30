671f85336a1426a4ea88b1e5e7e659aabb5b2891
AIE\s171496 <xeno.spam1993@gmail.com>
2018-07-30 10:03:55 +0930

    Previous repo files.

---
+++ Assets/Scripts/CaptureZoneActor.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptureZoneActor : MonoBehaviour {
+
+    public enum Owner { none, team1, team2 };
+
+    public float capturePercentage;
+
+    public Owner owner;
+
+    public float captureTimer = 0;
+    public float captureTime = 10;
+
+    List<TankActor> team1Tanks = new List<TankActor>();
+    List<TankActor> team2Tanks = new List<TankActor>();
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(capturePercentage == 0)
+        {
+            owner = Owner.none;
+        }
+
+        if(team1Tanks.Count > 0 && team2Tanks.Count == 0)
+        {
+            if (owner == Owner.none)
+            {
+                captureTimer -= Time.deltaTime;
+
+                if (captureTimer <= 0)
+                {
+                    capturePercentage += 10;
+                    captureTimer = captureTime;
+
+                    if (capturePercentage >= 100)
+                    {
+                        owner = Owner.team1;
+                    }
+                }
+            }
+            else if(owner == Owner.team2)
+            {
+                captureTimer -= Time.deltaTime;
+
+                if (captureTimer <= 0)
+                {
+                    capturePercentage -= 10;
+                    captureTimer = captureTime;
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        if (team2Tanks.Count > 0 && team1Tanks.Count == 0)
+        {
+            if (owner == Owner.none)
+            {
+                captureTimer -= Time.deltaTime;
+
+                if (captureTimer <= 0)
+                {
+                    capturePercentage += 10;
+                    captureTimer = captureTime;
+
+                    if (capturePercentage >= 100)
+                    {
+                        owner = Owner.team2;
+                    }
+                }
+            }
+            else if (owner == Owner.team1)
+            {
+                captureTimer -= Time.deltaTime;
+
+                if (captureTimer <= 0)
+                {
+                    capturePercentage -= 10;
+                    captureTimer = captureTime;
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+	}
+
+    private void OnTriggerEnter(Collider other) {
+
+        if(other.GetComponent<TankActor>().m_team1unit && other.GetComponent<TankActor>().m_isGeneral)
+        {
+            team1Tanks.Add(other.GetComponent<TankActor>());
+        }
+
+        if (other.GetComponent<TankActor>().m_team2unit && other.GetComponent<TankActor>().m_isGeneral)
+        {
+            team2Tanks.Add(other.GetComponent<TankActor>());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.GetComponent<TankActor>().m_team1unit && other.GetComponent<TankActor>().m_isGeneral)
+        {
+            team1Tanks.Remove(other.GetComponent<TankActor>());
+        }
+
+        if (other.GetComponent<TankActor>().m_team2unit && other.GetComponent<TankActor>().m_isGeneral)
+        {
+            team2Tanks.Remove(other.GetComponent<TankActor>());
+        }
+    }
+}
---
+++ Assets/Scripts/SquadController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SquadController : MonoBehaviour {
+
+    public List<TankActor> m_squad = new List<TankActor>();
+
+    public TankActor m_currentGeneral;
+
+    public TankActor m_enemy;
+
+    // Use this for initialization
+    void Start () {
+
+        m_squad = GetComponentsInChildren<TankActor>().ToList();
+
+        SetGeneral();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (m_squad.Count > 0)
+        {
+            foreach (TankActor tank in m_squad.ToList())
+            {
+                if (!tank.m_isGeneral)
+                    tank.FollowGeneral(m_currentGeneral);
+
+                if (m_enemy != null)
+                {
+                    tank.AttackEnemy(m_enemy);
+                }
+
+                if (m_enemy == null)
+                {
+                    if (!tank.m_isGeneral)
+                        tank.m_agent.stoppingDistance = 5.0f;
+                    else
+                        tank.m_agent.stoppingDistance = 0.0f;
+                }
+
+                if (tank.GetHealth() <= 0)
+                {
+                    if (!tank.m_isGeneral)
+                    {
+                        m_squad.Remove(tank);
+                        Destroy(tank.gameObject);
+                    }
+                    else if (tank.m_isGeneral)
+                    {
+                        m_squad.Remove(tank);
+                        PromoteToGeneral();
+                        SetGeneral();
+                        Destroy(tank.gameObject);
+                    }
+                }
+            }
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void setEnemy(TankActor enemy)
+    {
+        m_enemy = enemy;
+    }
+
+    void PromoteToGeneral()
+    {
+        foreach (TankActor tank in m_squad.ToList())
+        {
+            if(!tank.m_isGeneral)
+            {
+                tank.m_isGeneral = true;
+                return;
+            }
+        }
+    }
+
+    void SetGeneral()
+    {
+        foreach(TankActor tank in m_squad.ToList())
+        {
+            if(tank.m_isGeneral)
+            {
+                m_currentGeneral = tank;
+            }
+        }
+    }
+}
